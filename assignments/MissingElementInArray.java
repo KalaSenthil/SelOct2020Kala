1aad691bd83835be6b581f56f92678eeab147b93
KalaSenthil <72850325+KalaSenthil@users.noreply.github.com>
2020-10-14 11:45:43 +0530

    Update MissingElementInArray.java

--- assignments/MissingElementInArray.java
+++ assignments/MissingElementInArray.java
@@ -6,17 +6,10 @@ public class MissingElementInArray {
 
 	public static void main(String[] args) {
 		int arr[] = {1,2,3,4,7,6,8};
-		int total = 0;
-		int missnum;
-		int largestnum;
-		int length = arr.length;
-		int[] newArray = arr.clone();
 		
-		int newLength = newArray.length;
-		System.out.println("New Cloned Array");
-		for(int i=0;i<length;i++)
-			System.out.print(newArray[i]+",");
-		System.out.println();
+		int length = arr.length;
+		
+		
 		System.out.println("Array Length: "+length);
 		System.out.println("Print Original Array");
 		for(int i=0;i<length;i++)
@@ -28,12 +21,7 @@ public class MissingElementInArray {
 		{
 			System.out.print(arr[i]+",");
 		}
-		largestnum = arr[length-1];
-		
 		System.out.println();
-		System.out.println("largest number: "+largestnum);
-		
-		
 		for(int i=1;i<length;i++)
 		{
 			if((i)!=arr[i-1])
@@ -43,22 +31,7 @@ public class MissingElementInArray {
 			}			
 			
 		}
-		/*for(int i=0;i<length;i++)
-		{
-			total=total+arr[i];
-			
-		}
-		
-		System.out.println("Array Total: "+total);	
-		
-		
-		missnum = ((largestnum*(largestnum+1))/2)-total;
-		
-		System.out.println("Missing Number: "+missnum);
-		*/
-		
 				
-		
 	}
 
 }
